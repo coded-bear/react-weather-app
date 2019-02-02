b11dce9ce222f2e03aaa5647cf18bea60d33912b
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2019-02-02 18:13:41 +0100

    Added react-router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, browserHistory } from "react-router";
+
 import App from "./components/App/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route exact path="/" component={App} />
+  </Router>,
+  document.getElementById("root")
+);
