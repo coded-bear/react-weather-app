78fb9afc1ac5bafa2cddfde7c4c5519dfb7bf552
Łukasz Warchoł <lukasz.warchol14@gmail.com>
2019-02-03 13:03:19 +0100

    added search form

--- src/components/SearchEngine/index.js
+++ src/components/SearchEngine/index.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
 
 class SearchEngine extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ search: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.search);
+  };
+
   render() {
-    return <div className="SearchEngine">SearchEngine</div>;
+    return (
+      <div className="SearchEngine">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="Wyszukaj miasta..."
+          />
+          <input type="submit" value="Szukaj" />
+        </form>
+      </div>
+    );
   }
 }
 
